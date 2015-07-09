5315ec2bf36e8629a1f480ccb6815bac68ad30a8
Qiang Ji <qiang.ji@yahoo.com.au>
2015-07-09 23:42:53 +1000

    Make fixperms.

---
+++ vagrant/fixperms.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ "$(id -u)" != "0" ]; then
+  echo "fixperms must be run with sudo" 1>&2
+  exit 1
+fi
+
+CURDIR=`pwd`
+pwd | grep '^/var/www/html' > /dev/null
+
+if [ $? -eq 0 ]
+then
+  chown www-data:www-data $CURDIR/* $CURDIR/.[a-zA-Z0-9_]* -R 2>&1 | grep -v "No such"
+  chmod g+rw $CURDIR/* $CURDIR/.[a-zA-Z0-9_]* -R 2>&1 | grep -v "No such"
+  exit 0
+else
+  echo "Oops! You just tried to run fixperms in $CURDIR, which is not a subdirectory of /var/www. You're fired!"
+  exit 1
+fi
